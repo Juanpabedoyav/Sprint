ef77a6ceff9948c9009c28682a2227f78b459c0d
Juan Pablo Bedoya Vasco <bedoyavascojuanpablo@gmail.com>
2021-10-28 11:16:41 -0500

    boton editar de usuario

--- scripts/script.js
+++ scripts/script.js
@@ -1,100 +1,118 @@
 // conecct de API
-const URL = 'https://raw.githubusercontent.com/jennymontoya1001/endpointheroesjson/main/heroes.json'; 
-const obtenerData = async ()=>{
-const res = await fetch(URL);
-const data = await res.json();   
-const {results} = data; 
-return results;
-// descomposicion de data
-// console.log(results);
-}
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'http://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
+const principal = document.getElementById("principal");
 
-// bottoms
-const btnSearch = document.getElementById('btnSearch');
-// 
+// get data
+const getData = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  const { results } = data;
+  showMovies(data.results);
+};
 
-// Event listeners 
+// bottoms/////////////////////
+const btnSearch = document.getElementById("btnSearch");
+//
+
+// Event listeners
 
 // Event Search
-btnSearch.addEventListener('click', async () =>{
-    const search = document.getElementById('movie-search').value;
-    const data = await obtenerData();
-    let letterSearch = search.substring(0, 4);
-    let getSearch = data.filter(movie =>(movie.superhero.toLowerCase() === search.toLowerCase()) || (movie.publisher.toLowerCase() === search.toLowerCase()));
-    console.log(data);
-        // console.log(JSON.parse(getSearch));
+// btnSearch.addEventListener("click", async () => {
+//   const search = document.getElementById("movie-search").value;
+//   const data = await getData();
+//   let letterSearch = search.substring(0, 4);
+//   let getSearch = data.filter(
+//     (movie) =>
+//       movie.superhero.toLowerCase() === search.toLowerCase() ||
+//       movie.publisher.toLowerCase() === search.toLowerCase()
+//   );
+//   console.log(data);
+//   // console.log(JSON.parse(getSearch));
 
-    //  data.forEach(movie => {
-    //     let {
-    //         superhero,
-    //         name,
-    //         publisher,
-    //         alter_ego,
-    //         image,
-    //     } = movie;
-    //     if (superhero.substring(0, 4) === letterSearch.substring(0, 4) ){
-    //     console.log(letterSearch);
-    // }    
-    // });
-});
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+//   //  data.forEach(movie => {
+//   //     let {
+//   //         superhero,
+//   //         name,
+//   //         publisher,
+//   //         alter_ego,
+//   //         image,
+//   //     } = movie;
+//   //     if (superhero.substring(0, 4) === letterSearch.substring(0, 4) ){
+//   //     console.log(letterSearch);
+//   // }
+//   // });
+// });
 
 
 
-const url = 'https://raw.githubusercontent.com/jennymontoya1001/endpointheroesjson/main/heroes.json';
 
-const principal = document.getElementById('principal');
 
-const getData = async () => {
-    const res = await fetch(url);
-    const data = await res.json();
+
+function showMovies(movie) {
+  movie.forEach((movies) => {
     const {
-        results
-    } = data;
-    return results;
-    // descomposicion de data
-    // console.log(results);
+      original_title,
+      overview,
+      popularity,
+      poster_path,
+      title,
+      vote_average
+    } = movies;
+    const info = document.createElement("div");
+    info.classList.add("card");
+    info.innerHTML = `
+        <div class="card-content ">
+       <a id="item"> <img src="${IMG_PATH + poster_path}" alt=></a>
+          </div>
+      `;
+    principal.appendChild(info);
+  });
 }
 
-const showMovies = async() => {
-    let data = await getData();
-    data.forEach(movies => {
-        const {
-            superhero,
-            publisher,
-            name,
-            image
-        } = movies;
-    const info = document.createElement('div');
-    info.classList.add('card');
-        info.innerHTML = `
-        <div class="card-content ">
-       <a id="item"href="#"> <img src="${image}" alt=></a>
-          </div>
-      `
-principal.appendChild(info);
+const todas = document.getElementById("todas");
+const menosV = document.getElementById("menosV");
+const masV = document.getElementById("masV");
+
+menosV.addEventListener("click", async () => {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    const{results} = data;
+    const navFiltro = document.getElementById('navFiltro');
+    let filtro = results.filter(movie => (movie.vote_average < 7))
+
+    navFiltro.innerHTML = "";
+    navFiltro.innerHTML +=
+
+    `  <p class="title">Menos Valoradas</p> 
+    <div id="principal"></div>
+   
+    `
+    console.log(filtro);
+})
+    
+
+masV.addEventListener("click", async () => {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    const{results} = data;
+    const navFiltro = document.getElementById('navFiltro');
+    let filtro = results.filter(movie => (movie.vote_average < 7))
+
+    navFiltro.innerHTML = "";
+    navFiltro.innerHTML +=
+
+    `  <p class="title">Más Valoradas</p> 
+    <div id="principal"></div>
+   
+    `
+    console.log(filtro);
+})    
 
 
-// modal
-// const item = document.querySelector('#item');
-// const modalBackground = document.querySelector('.modal-background');
-// const modal = document.querySelector('.modal');
 
-// item.addEventListener('click', ()=>{
-//     modal.classList.add('is-active');
-// console.log(item);
-
-// })
-
-
-
-
-
- });
-};
-showMovies();
-
-
+getData(API_URL);
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,9 +1,9 @@
 // URLS SERVERS
-const url = 'http://localhost:4000/newUser' ;
+const url = 'http://localhost:4000/newUser';
 
 
 // botton
-const form = document.getElementById('form');
+const form = document.querySelector('.form');
 
 // links rediretion
 
@@ -19,38 +19,129 @@ form.addEventListener('submit', async (e) => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
-// Send data to signup.json
+    // Send data to signup.json add user
     await fetch(url, {
-    method : "POST",
-    body: JSON.stringify({
-        name,
-        userName,
-        email, 
-        password 
-    }),
-    headers:
-    {"Content-Type": "application/json; charset=UTF-8" },
-    });    
-      // document.getElementById('name').required = true;
+        method: "POST",
+        body: JSON.stringify({
+            name,
+            userName,
+            email,
+            password
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+    });
 
- // Load main page then of signup 
- if (name != "" && userName!= "" && email!= "" && password!= ""){
-    console.log("Registro exitoso bienvenido");
-    alert("Registro Exitoso Bienvenido");
-    window.open("index.html");
-    setTimeout(window.open("index.html", 8000));
-    window.close("signup.html");
-};
+    // Load main page then of signup 
+    if (name != "" && userName != "" && email != "" && password != "") {
+        console.log("Registro exitoso bienvenido");
+        alert("Registro Exitoso Bienvenido");
+        window.open("index.html");
+        setTimeout(window.open("index.html", 8000));
+    };
 });
 
 // Event login
-login.addEventListener('click', (e)=> {
+login.addEventListener('click', (e) => {
     e.preventDefault();
-    let container = document.getElementById('container');
-    let form = document.createElement('form');
-    container.innerHTML = "";
-
+    // let fieldEmail = document.querySelector('.field--email');
+    form.innerHTML = "";
     form.classList.add('form');
-    // form.innerHTML `  `;
+    form.innerHTML += `
+    <h1 class="verificar-email">Verifica tu cuenta</h1>
+    <div class="field field--email">
+    <label for="email" class="label">Correo eletrónico</label>
+    <p class="control has-icons-left">
+     <input id="email" class="input is-normal" type="email"
+                            placeholder="Ingresa tu correo eletrónico" required>
+        <span class="icon is-left">
+            <i class="fas fa-envelope"></i>
+                </span>
+            </div>  
+        <p class="control">
+        <button id="btnVerificar" class="button is-normal type="button">Verificar Email</button>
+        </p>
+        <p class="back">
+        <a id="back" class="link" href="signup.html">Volver</a>
+     </p>
+    
+    `
+    const btnVerificar= document.getElementById('btnVerificar');
+    btnVerificar.addEventListener('click', async()=>{
+    
+        let inputEmail = document.querySelector('input[type="email"]').value;
 
+      const res = await fetch (url);
+      const data = await res.json();
+      
+      let user = data.find(usuario => (usuario.email == inputEmail))
+        const{name, userName, email} = user;
+
+
+         if(user = true){
+            const field = document.querySelector('.field');
+            field.innerHTML = "";
+            field.innerHTML += `
+            <div class="field field--email">
+            <label for="email" class="label">Correo eletrónico</label>
+            <p class="control has-icons-left">
+                <input id="email" class="input is-normal" type="email"
+                    placeholder="Ingresa tu correo eletrónico" required>
+                <span class="icon is-left">
+                    <i class="fas fa-envelope"></i>
+                </span>
+            </p>   
+            </div>
+            <div class="field">
+                    <label for="name" class="label">Nombre</label>
+                    <p class="control has-icons-left">
+                        <input id="name" class="input is-normal" type="text" placeholder="Ingresa tu nombre" required>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                    </p>
+                </div>
+                <div class="field">
+                <label for="userName" class="label">Apodo</label>
+                <p class="control has-icons-left">
+                    <input id="userName" class="input is-normal" type="text" placeholder="Ingresa tu apodo"
+                        required>
+                    <span class="icon  is-left">
+                        <i class="fas fa-at"></i>
+                    </span>
+                </p>
+            </div>
+            <p class="control">
+            <button id="btnEdit" class="button is-normal type="botton">Editar</button>
+        </p>
+        <p class="control">
+                    <button id="btnSave" class="button is-normal" type="botton">Guardar Cambios</button>
+                </p>
+            `;
+          
+            
+            let inputEmail = document.getElementById('email').readOnly = true;
+            let inputName = document.getElementById('name').readOnly = true;
+            let inputApodo = document.getElementById('userName').readOnly = true;
+
+
+            document.querySelector('input[type="email"]').value= email;
+            document.getElementById('name').value= name;
+            document.getElementById('userName').value= userName;
+        }
+        // changes the profile  PUT AND DELETE
+        const btnEdit = document.getElementById('btnEdit');        
+        btnEdit.addEventListener('click',()=>{
+            let inputName = document.getElementById('name').readOnly = false;
+            let inputApodo = document.getElementById('userName').readOnly = false;
+        });
+        
+        
+        
+        
+        const btnSave = document.getElementById('btnSave');
+
+
+    })  
 })

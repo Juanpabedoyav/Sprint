05a1d7bbf98c0639cd38c9573ff0ed99b9d9457c
Juan Pablo Bedoya Vasco <bedoyavascojuanpablo@gmail.com>
2021-10-27 21:05:10 -0500

    se crea template

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,39 @@
-const URL = 'https://raw.githubusercontent.com/jennymontoya1001/endpointheroesjson/main/heroes.json'; 
-const obtenerData = async ()=>{
-const res = await fetch(URL);
-const data = await res.json();   
-const {results} = data; 
-return results;
-// descomposicion de data
-// console.log(results);
+const url = 'https://raw.githubusercontent.com/jennymontoya1001/endpointheroesjson/main/heroes.json';
+
+const principal = document.getElementById('principal');
+
+const getData = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    const {
+        results
+    } = data;
+    return results;
+    // descomposicion de data
+    // console.log(results);
 }
 
+const showMovies = async() => {
+    let data = await getData();
+    data.forEach(movies => {
+        const {
+            superhero,
+            publisher,
+            name,
+            image
+        } = movies;
+    const info = document.createElement('div');
+    info.classList.add('card');
+        info.innerHTML = `
+        
+          <div class="card-content ">
+            <img src="${image}" alt=>
+          </div>
+        
+      `
+      principal.appendChild(info);
 
+    });
+
+};
+showMovies();
